41806e3b602712aa284cd8abc4f89f21f56a04bb
cc <1063329659@qq.com>
2019-04-04 15:57:27 +0800

    第一次上传

---
+++ src/main/java/com/java/spring_buto/SpringButoApplication.java
@@ -0,0 +1,13 @@
+package com.java.spring_buto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringButoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringButoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/java/spring_buto/dd.java
@@ -0,0 +1,7 @@
+package com.java.spring_buto;
+
+public class dd {
+    public static void main(String[] args) {
+        System.out.println("...");
+    }
+}
